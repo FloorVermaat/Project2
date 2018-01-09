8418c1e8422e7e9968f135655076696a703f4400
Jurre <Jurre+Spam@Koetse.eu>
2018-01-09 13:59:57 +0100

    Added Space Race

---
+++ SR/main.py
@@ -0,0 +1,481 @@
+import pygame as pg
+import sys
+from os import path
+vec = pg.math.Vector2
+
+# ---------SETTINGS---------
+# define some colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# game settings
+WIDTH = 1280
+HEIGHT = 720
+FPS = 30
+TITLE = "Thijs' game"
+BGCOLOR = BLACK
+
+TILESIZE = 48
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+POINTS_GIVEN = 1
+POINT_IMG = 'point.png'
+WALL_IMG = 'wall.png'
+
+# Player settings
+PLAYER_HEALTH = 100
+PLAYER_SPEED = 300
+PLAYER_ROT_SPEED = 200
+PLAYER_IMG = 'spaceship.png'
+PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
+BARREL_OFFSET = vec(30, 10)
+
+# Mob settings
+MOB_IMG = 'spaceship2.png'
+MOB_SPEED = 350
+MOB_HIT_RECT = pg.Rect(0, 0, 30, 30)
+MOB_HEALTH = 100
+MOB_DAMAGE = 10
+MOB_KNOCKBACK = 20
+
+game_folder = path.dirname(__file__)
+img_folder = path.join(game_folder, 'img')
+sound_folder = path.join(game_folder, 'sound')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ---------SPRITES---------
+def collide_with_walls(sprite, group, dir):
+    if dir == 'x':
+        hits = pg.sprite.spritecollide(sprite, group, False, collide_hit_rect)
+        if hits:
+            if hits[0].rect.centerx > sprite.hit_rect.centerx:
+                sprite.pos.x = hits[0].rect.left - sprite.hit_rect.width / 2
+            if hits[0].rect.centerx < sprite.hit_rect.centerx:
+                sprite.pos.x = hits[0].rect.right + sprite.hit_rect.width / 2
+            sprite.vel.x = 0
+            sprite.hit_rect.centerx = sprite.pos.x
+    if dir == 'y':
+        hits = pg.sprite.spritecollide(sprite, group, False, collide_hit_rect)
+        if hits:
+            if hits[0].rect.centery > sprite.hit_rect.centery:
+                sprite.pos.y = hits[0].rect.top - sprite.hit_rect.height / 2
+            if hits[0].rect.centery < sprite.hit_rect.centery:
+                sprite.pos.y = hits[0].rect.bottom + sprite.hit_rect.height / 2
+            sprite.vel.y = 0
+            sprite.hit_rect.centery = sprite.pos.y
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.player_img
+        self.rect = self.image.get_rect()
+        self.hit_rect = PLAYER_HIT_RECT
+        self.hit_rect.center = self.rect.center
+        self.vel = vec(0, 0)
+        self.pos = vec(x, y) * TILESIZE
+        self.rot = 0
+        self.last_shot = 0
+        self.health = PLAYER_HEALTH
+
+    def get_keys(self):
+        self.rot_speed = 0
+        self.vel = vec(0, 0)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.rot_speed = PLAYER_ROT_SPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.rot_speed = -PLAYER_ROT_SPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vel = vec(PLAYER_SPEED, 0).rotate(-self.rot)
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vel = vec(-PLAYER_SPEED / 2, 0).rotate(-self.rot)
+
+    def update(self):
+        self.get_keys()
+        self.rot = (self.rot + self.rot_speed * self.game.dt) % 360
+        self.image = pg.transform.rotate(self.game.player_img, self.rot)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+        self.pos += self.vel * self.game.dt
+        self.hit_rect.centerx = self.pos.x
+        collide_with_walls(self, self.game.walls, 'x')
+        self.hit_rect.centery = self.pos.y
+        collide_with_walls(self, self.game.walls, 'y')
+        self.rect.center = self.hit_rect.center
+
+class Mob(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.mobs
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.mob_img
+        self.rect = self.image.get_rect()
+        self.hit_rect = MOB_HIT_RECT.copy()
+        self.hit_rect.center = self.rect.center
+        self.pos = vec(x, y) * TILESIZE
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.rect.center = self.pos
+        self.rot = 0
+        self.health = MOB_HEALTH
+
+    def update(self):
+        self.rot = (self.game.player.pos - self.pos).angle_to(vec(1, 0))
+        self.image = pg.transform.rotate(self.game.mob_img, self.rot)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+        self.acc = vec(MOB_SPEED, 0).rotate(-self.rot)
+        self.acc += self.vel * -1
+        self.vel += self.acc * self.game.dt
+        self.pos += self.vel * self.game.dt + 0.5 * self.acc * self.game.dt ** 2
+        self.hit_rect.centerx = self.pos.x
+        collide_with_walls(self, self.game.walls, 'x')
+        self.hit_rect.centery = self.pos.y
+        collide_with_walls(self, self.game.walls, 'y')
+        self.rect.center = self.hit_rect.center
+
+    def draw_health(self):
+        if self.health > 67:
+            col = GREEN
+        elif self.health > 34:
+            col = YELLOW
+        else:
+            col = RED
+        width = int(self.rect.width * self.health / MOB_HEALTH)
+        self.health_bar = pg.Rect(0, 0, width, 7)
+        if self.health < MOB_HEALTH:
+            pg.draw.rect(self.image, col, self.health_bar)
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.wall_img
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
+class Point(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.points
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.point_img
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ---------MAP---------
+def collide_hit_rect(one, two):
+    return one.hit_rect.colliderect(two.rect)
+
+class Map:
+    def __init__(self, filename):
+        self.data = []
+        with open(filename, 'rt') as f:
+            for line in f:
+                self.data.append(line.strip())
+
+        self.tilewidth = len(self.data[0])
+        self.tileheight = len(self.data)
+        self.width = self.tilewidth * TILESIZE
+        self.height = self.tileheight * TILESIZE
+
+class Camera:
+    def __init__(self, width, height):
+        self.camera = pg.Rect(0, 0, width, height)
+        self.width = width
+        self.height = height
+
+    def apply(self, entity):
+        return entity.rect.move(self.camera.topleft)
+
+    def update(self, target):
+        x = -target.rect.centerx + int(WIDTH / 2)
+        y = -target.rect.centery + int(HEIGHT / 2)
+
+        # limit scrolling to map size
+        x = min(0, x)  # left
+        y = min(0, y)  # top
+        x = max(-(self.width - WIDTH), x)  # right
+        y = max(-(self.height - HEIGHT), y)  # bottom
+        self.camera = pg.Rect(x, y, self.width, self.height)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ---------MAIN---------
+# HUD functions
+def draw_player_health(surf, x, y, pct):
+    if pct < 0:
+        pct = 0
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 20
+    fill = pct * BAR_LENGTH
+    outline_rect = pg.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pg.Rect(x, y, fill, BAR_HEIGHT)
+    if pct > 0.67:
+        col = GREEN
+    elif pct > 0.34:
+        col = YELLOW
+    else:
+        col = RED
+    pg.draw.rect(surf, col, fill_rect)
+    pg.draw.rect(surf, WHITE, outline_rect, 2)
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.load_data()
+
+    def draw_text(self, text, font_name, size, color, x, y, align="nw"):
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        if align == "nw":
+            text_rect.topleft = (x, y)
+        if align == "ne":
+            text_rect.topright = (x, y)
+        if align == "sw":
+            text_rect.bottomleft = (x, y)
+        if align == "se":
+            text_rect.bottomright = (x, y)
+        if align == "n":
+            text_rect.midtop = (x, y)
+        if align == "s":
+            text_rect.midbottom = (x, y)
+        if align == "e":
+            text_rect.midright = (x, y)
+        if align == "w":
+            text_rect.midleft = (x, y)
+        if align == "center":
+            text_rect.center = (x, y)
+        self.screen.blit(text_surface, text_rect)
+
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        img_folder = path.join(game_folder, 'img')
+        sound_folder = path.join(game_folder, 'sound')
+        self.title_font = path.join(img_folder, 'ALBA.TTF')
+        self.dim_screen = pg.Surface(self.screen.get_size()).convert_alpha()
+        self.dim_screen.fill((0, 0, 0, 180))
+        self.map = Map(path.join(game_folder, 'map2.txt'))
+        #player image
+        self.player_img = pg.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
+        #mob image
+        self.mob_img = pg.image.load(path.join(img_folder, MOB_IMG)).convert_alpha()
+        #wall image
+        self.wall_img = pg.image.load(path.join(img_folder, WALL_IMG)).convert_alpha()
+        self.wall_img = pg.transform.scale(self.wall_img, (TILESIZE, TILESIZE))
+        #point image
+        self.point_img = pg.image.load(path.join(img_folder, POINT_IMG)).convert_alpha()
+        self.point_img = pg.transform.scale(self.point_img, (12, 12))
+
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.mobs = pg.sprite.Group()
+        self.points = pg.sprite.Group()
+        self.score = 0
+        for row, tiles in enumerate(self.map.data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == 'M':
+                    Mob(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
+                if tile == '.':
+                    Point(self, col, row)
+        self.camera = Camera(self.map.width, self.map.height)
+        self.paused = False
+
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000.0  # fix for Python 2.x
+            self.events()
+            if not self.paused:
+                self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
+        self.camera.update(self.player)
+        # mobs hit player
+        hits = pg.sprite.spritecollide(self.player, self.mobs, False, collide_hit_rect)
+        for hit in hits:
+            self.player.health -= MOB_DAMAGE
+            hit.vel = vec(0, 0)
+            if self.player.health <= 0:
+                self.playing = False
+        # Player hits point
+        hits = pg.sprite.spritecollide(self.player, self.points, False, collide_hit_rect)
+        for hit in hits:
+            self.score += POINTS_GIVEN
+            if self.score > 3000:
+                g.show_win_screen()
+
+    def draw(self):
+        pg.display.set_caption("{:.2f}".format(self.clock.get_fps()))
+        self.screen.fill(BGCOLOR)
+        # self.draw_grid()
+        for sprite in self.all_sprites:
+            if isinstance(sprite, Mob):
+                sprite.draw_health()
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
+        # HUD functions
+        draw_player_health(self.screen, 10, 10, self.player.health / PLAYER_HEALTH)
+        self.draw_text(str(self.score), self.title_font, 50, RED, 1175, 15)
+        if self.paused:
+            self.screen.blit(self.dim_screen, (0, 0))
+            self.draw_text("Paused", self.title_font, 105, RED, WIDTH / 2, 200, align="center")
+            self.draw_text("Press SPACEBAR to continue", self.title_font, 75, WHITE,
+                           WIDTH / 2, 350, align="center")
+            self.draw_text("Press ESCAPE to quit", self.title_font, 75, WHITE,
+                           WIDTH / 2, 500, align="center")
+        pg.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.paused = not self.paused
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+
+    def show_start_screen(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_text("SPACE RACE", self.title_font, 200, RED,
+                       WIDTH / 2, 200, align="center")
+        self.draw_text("Use WASD or ARROW keys to move", self.title_font, 50, WHITE
+                       , WIDTH / 2, 350, align="center")
+        self.draw_text("Press SPACEBAR to pause the game", self.title_font, 50, WHITE,
+                       WIDTH / 2, 425, align="center")
+        self.draw_text("Press ESCAPE to quit", self.title_font, 50, WHITE,
+                       WIDTH / 2, 500, align="center")
+        self.draw_text("Press a key to start", self.title_font, 50, WHITE,
+                       WIDTH / 2, 575, align="center")
+        pg.display.flip()
+        self.wait_for_key()
+
+
+    def show_go_screen(self):
+        self.screen.fill(BLACK)
+        self.draw_text("GAME OVER", self.title_font, 200, RED,
+                       WIDTH / 2, 300, align="center")
+        self.draw_text("Press A KEY to try again", self.title_font, 75, WHITE,
+                       WIDTH / 2, 450, align="center")
+        pg.display.flip()
+        self.wait_for_key()
+
+
+    def show_win_screen(self):
+        self.screen.fill(BLACK)
+        self.draw_text("YOU WIN", self.title_font, 200, RED,
+                       WIDTH / 2, 300, align='center')
+        self.draw_text("Press A KEY to quit the game", self.title_font, 75, WHITE,
+                       WIDTH / 2, 450, align="center")
+        pg.display.flip()
+        self.wait_for_escape()
+        self.score = 0
+
+    def wait_for_key(self):
+        pg.event.wait()
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.quit()
+                if event.type == pg.KEYUP:
+                    waiting = False
+
+    def wait_for_escape(self):
+        pg.event.wait()
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.quit()
+                if event.type == pg.KEYUP:
+                    waiting = False
+                    self.quit()
+
+# create the game object
+g = Game()
+g.show_start_screen()
+pg.mixer.music.load(path.join(sound_folder, 'main.mp3'))
+pg.mixer.music.play(-1)
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
+
