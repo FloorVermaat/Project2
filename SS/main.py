8b6f8fcfd169de339f9915ec1b148f374fa3a845
Maarten <maarten847@gmail.com>
2018-01-14 22:01:52 +0100

    draaiende astroide + sneller + achtergrond + kogels blijven langer en je kan jezelf raken

--- SS/main.py
+++ SS/main.py
@@ -230,8 +230,9 @@ def draw_player_health(surf, x, y, pct):
     pg.draw.rect(surf, WHITE, outline_rect, 2)
 
 class Game:
-    def __init__(self, screen):
+    def __init__(self, screen, story):
         self.screen = screen
+        self.story = story
         self.clock = pg.time.Clock()
         pg.key.set_repeat(500, 100)
         self.load_data()
@@ -318,26 +319,31 @@ class Game:
             self.events()
             if not self.paused:
                 self.update()
-            # if mode = 'story':
-                #if self.score > 2500:
+            if self.story == True:
+                if self.score >= 0:
+                    while len(self.mobs) < 6:
+                        Mob(self, random.randrange(0, WIDTH), random.randrange(0, HEIGHT))
+                        self.score += MOB_SCORE
+                if self.score >= 100:
+                    self.playing = False
                     #you win
-            # if mode = 'survival':
-            if self.score >= 0:
-                while len(self.mobs) < 6:
-                    Mob(self, random.randrange(0, WIDTH), random.randrange(0, HEIGHT))
-                    self.score += MOB_SCORE
-            if self.score > 1000:
-                while len(self.mobs) < 12:
-                    Mob(self, random.randrange(0, WIDTH), random.randrange(0, HEIGHT))
-                    self.score += MOB_SCORE
-            if self.score > 2500:
-                while len(self.mobs) < 18:
-                    Mob(self, random.randrange(0, WIDTH), random.randrange(0, HEIGHT))
-                    self.score += MOB_SCORE
-            if self.score > 4000:
-                while len(self.mobs) < 26:
-                    Mob(self, random.randrange(0, WIDTH), random.randrange(0, HEIGHT))
-                    self.score += MOB_SCORE
+            if self.story == False:
+                if self.score >= 0:
+                    while len(self.mobs) < 6:
+                        Mob(self, random.randrange(0, WIDTH), random.randrange(0, HEIGHT))
+                        self.score += MOB_SCORE
+                if self.score > 1000:
+                    while len(self.mobs) < 12:
+                        Mob(self, random.randrange(0, WIDTH), random.randrange(0, HEIGHT))
+                        self.score += MOB_SCORE
+                if self.score > 2500:
+                    while len(self.mobs) < 18:
+                        Mob(self, random.randrange(0, WIDTH), random.randrange(0, HEIGHT))
+                        self.score += MOB_SCORE
+                if self.score > 4000:
+                    while len(self.mobs) < 26:
+                        Mob(self, random.randrange(0, WIDTH), random.randrange(0, HEIGHT))
+                        self.score += MOB_SCORE
             self.draw()
 
     def quit(self):
@@ -436,7 +442,7 @@ class Game:
                        WIDTH / 2, 450, align="center")
         self.draw_text("score = " + str(self.score), self.title_font, 75, WHITE,
                        WIDTH / 2, 550, align="center")
-        self.draw_text("Press ESC or R to quit", self.title_font, 75, WHITE,
+        self.draw_text("Press ESC or Q to quit", self.title_font, 75, WHITE,
                       WIDTH / 2, 650, align="center")
         pg.display.flip()
         self.wait_for_key()
@@ -446,12 +452,10 @@ class Game:
         self.screen.fill(BLACK)
         self.draw_text("You WIN!!!", self.title_font, 200, RED,
                        WIDTH / 2, 300, align="center")
-        self.draw_text("Press R KEY to try again", self.title_font, 75, WHITE,
-                       WIDTH / 2, 450, align="center")
         self.draw_text("score = " + str(self.score), self.title_font, 75, WHITE,
+                       WIDTH / 2, 450, align="center")
+        self.draw_text("Press ESC or Q to quit", self.title_font, 75, WHITE,
                        WIDTH / 2, 550, align="center")
-        self.draw_text("Press ESC or R to quit", self.title_font, 75, WHITE,
-                       WIDTH / 2, 650, align="center")
         pg.display.flip()
         self.wait_for_key()
         # pass
@@ -475,12 +479,15 @@ class Game:
 def SS(screen, story):
     print(story)
     # create the game object
-    g = Game(screen)
+    g = Game(screen, story)
     g.show_start_screen()
     # pg.mixer.music.load(path.join(sound_folder, 'music.mp3'))
     # pg.mixer.music.play(-1)
     while g.running:
         g.new()
         g.run()
-        g.show_go_screen()
+        if story == True:
+            g.show_win_screen()
+        if story == False:
+            g.show_go_screen()
 
